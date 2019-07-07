809e2af021959970f063cbda4a98f681a0b4d16e
Sergey <necromongus@gmail.com>
2019-07-07 19:14:08 +0300

    alpha ver 0.1

--- js/main.js
+++ js/main.js
@@ -2,20 +2,15 @@ function castleStart(){
     const fill = document.querySelector('.fill');
     const empties = document.querySelectorAll('.empty');
 
-
-    // Fill listeners
     fill.addEventListener('dragstart', dragStart);
     fill.addEventListener('dragend', dragEnd);
 
-    // Loop through empty boxes and add listeners
     for (const empty of empties) {
     empty.addEventListener('dragover', dragOver);
     empty.addEventListener('dragenter', dragEnter);
     empty.addEventListener('dragleave', dragLeave);
     empty.addEventListener('drop', dragDrop);
     }
-
-    // Drag Functions
 
     function dragStart() {
     fillClasses = this.closest("td").classList;
@@ -26,7 +21,6 @@ function castleStart(){
 
     function dragEnd() {
     this.className = 'fill';
-    console.log(fillClasses)
     }
 
     function dragOver(e) {
@@ -35,31 +29,20 @@ function castleStart(){
 
     function dragEnter(e) {
     e.preventDefault();
-    // if (this.classList[1] === fillClasses[1]){
-    //     this.className += ' hovered';
-    // } else {
-    //     if (this.classList[2] === fillClasses[2]){
-    //         this.className += ' hovered';
-    //     } 
-    // }
     if (this.classList[1] === fillClasses[1] || this.classList[2] === fillClasses[2]) {
         this.className += ' hovered';
     } 
     }
 
     function dragLeave() {
-    // this.className = 'empty';
     this.classList.remove("hovered")    
     }
 
     function dragDrop() {
-    // this.className = 'empty';
     if (this.classList[1] === fillClasses[1] || this.classList[2] === fillClasses[2]) {
         this.classList.remove("hovered")  
         this.append(fill);
     } 
-    // this.classList.remove("hovered")  
-    // this.append(fill);
     }
 }
 
